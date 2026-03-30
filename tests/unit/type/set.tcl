2ebb6c1ab53d2fb7c1b68432d13ca074b28d997c
debing.sun <debing.sun@redis.com>
2026-03-30 19:50:47 +0800

    Test tcp deadlock fixes (#14946)
    This fix follows #14667 and #14886
    
    Several tests pipelined large numbers of commands on deferring clients
    without draining replies. That can fill buffers and stall progress.
    
    Fix by draining replies every 500 pipelined requests to avoid TCP
    stalls.
    
    ---------
    
    Co-authored-by: oranagra <oran@redislabs.com>

--- tests/unit/type/set.tcl
+++ tests/unit/type/set.tcl
@@ -71,7 +71,7 @@ start_server {
         r del zmscoretest
         r zadd zmscoretest 10 x
         r zadd zmscoretest 20 y
-        
+
         catch {r smismember zmscoretest} e
         assert_match {*ERR*wrong*number*arg*} $e
     }
@@ -1026,7 +1026,22 @@ foreach type {single multiple single_multiple} {
                 break
             }
         }
-        r srem $myset {*}$members
+        r deferred 1
+        set count 0
+        foreach m $members {
+            r srem $myset $m
+            incr count
+            if {$count == 500} {
+                for {set i 0} {$i < 500} {incr i} {
+                    r read
+                }
+                set count 0
+            }
+        }
+        for {set i 0} {$i < $count} {incr i} {
+            r read
+        }
+        r deferred 0
     }
 
     proc verify_rehashing_completed_key {myset table_size keys} {
@@ -1105,7 +1120,7 @@ foreach type {single multiple single_multiple} {
         #    otherwise we would need more iterations.
         rem_hash_set_top_N myset [expr {[r scard myset] - 30}]
         assert_equal [r scard myset] 30
-        
+
         # Hash set rehashing would be completed while removing members from the `myset`
         # We also check the size and members in the hash table.
         verify_rehashing_completed_key myset 64 30
